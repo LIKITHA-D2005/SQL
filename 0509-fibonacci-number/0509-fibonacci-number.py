caba1d1dbf84ffcf7a8cf10ce8e2b5ff1a746528
LIKITHA D <likithadinesh14@gmail.com>
2026-07-12 11:37:50 +0530

    Time: 425 ms (5.07%), Space: 19.2 MB (18.22%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def func(self,num):
+        if num == 0 or num ==1:
+            return num
+        return self.func(num-1) + self.fib(num-2)
+
+    def fib(self, n: int) -> int:
+        answer = self.func(n)
+        return answer
+
+    
+    
+        
